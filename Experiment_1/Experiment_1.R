8e1ecf74cfb01e1f41049faf6684aa5c53e5d518
Yannan Lin <37195634+allyn1982@users.noreply.github.com>
2022-05-31 16:55:48 -0700

    Update Experiment_1.R
    add comments

--- Experiment_1/Experiment_1.R
+++ Experiment_1/Experiment_1.R
@@ -1,6 +1,25 @@
-# experiment 1
+# experiment 1 
+# build a multivariable model to identify significant predictors of patient non-adherence to baseline Lung-RADS recommendations
+
+# Baseline predictors
+# Lung-RADS score: lungrads_12_3_4
+# Age:age_new
+# Sex: sex_new
+# Race/ethnicity: race_ethnicity_new
+# Education: education_new
+# Smoking status: smoking_status_new
+# Family history of lung cancer: fam_hx_lc_new
+# Primary insurance: insurance_new
+# Age-adjusted Charlson Comorbidity Index: comorbid_category_new_2
+# Distance to screening center: distance_to_center_category_new
+# ADI state rank: adi_category_new
+# Type of referring physician: department_new
+# Median family income: median_income_category_new
+# COVID pause period: covid_expected_fu_date_lungrads_interval_new
+
+
 multi_lr_model <- function(my_data){
-  # full model - multiviariable
+  # multiviariable logistic regression model
   model <- glm(adherence_altered ~ lungrads_12_3_4+age_new+
                  sex_new+race_ethnicity_new+
                  education_new+smoking_status_new+
@@ -10,7 +29,11 @@ multi_lr_model <- function(my_data){
                  covid_expected_fu_date_lungrads_interval_new,
                data = my_data,
                family = binomial(link="logit"))
+  
+  # print number of observations for the model
   print(nobs(model))
+  
+  # print coefficients and its 95% CI
   round(exp(cbind(coef(model), confint(model))), digits = 2)
 }
 
