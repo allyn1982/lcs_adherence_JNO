01c1ce6550e837d53e2f6b1cc198734ada45a30a
Yannan Lin <37195634+allyn1982@users.noreply.github.com>
2022-05-31 17:13:03 -0700

    Update dataloader.py
    add comments

--- Experiment_2/dataloader.py
+++ Experiment_2/dataloader.py
@@ -15,6 +15,8 @@ class DataLoader:
         self.file_type = file_type
 
     def get_data(self, model_type=None):
+        """Read data for logistic regression, SVM, random forest, and XGBoost classifiers
+        """
         data = pd.read_csv(self.file_path)
         # specify predictors for full and simple models
         if model_type == 'full':
@@ -26,18 +28,24 @@ class DataLoader:
         return X, y
 
     def get_data_nb(self):
+        """Read data for naive bayes classifiers
+        """
         data = pd.read_csv(self.file_path)
         if self.file_type == 'full_imputed':
+            # drop a redundant column titled 'unnamed'
             data = data.drop(data.columns[[0]], axis=1)
             X = data.iloc[:, :-1]
             y = data.iloc[:, -1]
         else:
+            # drop redundant columns titled 'unnamed' and 'id'
             data = data.drop(data.columns[[0, 1]], axis=1)
             X = data.iloc[:, :-1]
             y = data.iloc[:, -1]
         return X, y
 
     def get_test_data(self,  model_type=None, test_path=None):
+        """Read hold out test data
+        """
         test_data = pd.read_csv(test_path)
         if model_type == 'full':
             test_var_df = create_var_df(test_data)
