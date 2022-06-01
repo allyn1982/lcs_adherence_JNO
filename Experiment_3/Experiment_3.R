e2efb2f53c3ddbdb558581dcc0cae76fc6e9a65c
Yannan Lin <37195634+allyn1982@users.noreply.github.com>
2022-06-01 03:12:06 -0700

    Add files via upload
    add files

--- Experiment_3/Experiment_3.R
+++ Experiment_3/Experiment_3.R
@@ -2,6 +2,7 @@
 # investigate the association between changes in Lung-RADS scores and patient adherence across mulitple screening time points
 
 # Baseline predictors
+# id: research_id
 # Lung-RADS score: baseline_lr_category
 # Age: baseline_age
 # Sex: sex_new
@@ -26,54 +27,61 @@ library(gee)
 
 # extract 95% CI for coefficients for variables of interest only
 get_CI <- function(model){
-  print('Baseline LR 1-2, LR unchanged from T0 to T1, T0 vs T1')
+  print('Baseline LR 1to2, LR unchanged from T0 to T1, T0 vs T1')
   print(exp(coef(summary(model))['ldct_index2',1] + 
               qnorm(c(0.025, 0.975)) * coef(summary(model))['ldct_index2',4]))
-  print('Baseline LR 1-2, LR unchanged from T0 to T2, T0 vs T2')    
+  print('Baseline LR 1to2, LR unchanged from T0 to T2, T0 vs T2')    
   print(exp(coef(summary(model))['ldct_index3',1] + 
               qnorm(c(0.025, 0.975)) * coef(summary(model))['ldct_index3',4]))
-  print('Baseline LR 3-4, LR unchanged from T0 to T1, T0 vs T1')
-  print(exp(coef(summary(model))['baseline_lr_category3-4:ldct_index2',1] + 
-              qnorm(c(0.025, 0.975)) * coef(summary(model))['baseline_lr_category3-4:ldct_index2',4]))
-  print('Baseline LR 3-4, LR unchanged from T0 to T2, T0 vs T2')
-  print(exp(coef(summary(model))['baseline_lr_category3-4:ldct_index3',1] + 
-              qnorm(c(0.025, 0.975)) * coef(summary(model))['baseline_lr_category3-4:ldct_index3',4]))
-  print('Baseline LR 1-2, LR upgraded  from T0 to T1, T0 vs T1')
+  print('Baseline LR 3to4, LR unchanged from T0 to T1, T0 vs T1')
+  print(exp(coef(summary(model))['baseline_lr_category3to4:ldct_index2',1] + 
+              qnorm(c(0.025, 0.975)) * coef(summary(model))['baseline_lr_category3to4:ldct_index2',4]))
+  print('Baseline LR 3to4, LR unchanged from T0 to T2, T0 vs T2')
+  print(exp(coef(summary(model))['baseline_lr_category3to4:ldct_index3',1] + 
+              qnorm(c(0.025, 0.975)) * coef(summary(model))['baseline_lr_category3to4:ldct_index3',4]))
+  print('Baseline LR 1to2, LR upgraded  from T0 to T1, T0 vs T1')
   print(exp(coef(summary(model))['lr_change_modified_newchange:ldct_index2',1] + 
               qnorm(c(0.025, 0.975)) * coef(summary(model))['lr_change_modified_newchange:ldct_index2',4]))
-  print('Baseline LR 1-2, LR upgraded  from T0 to T2, T0 vs T2')
+  print('Baseline LR 1to2, LR upgraded  from T0 to T2, T0 vs T2')
   print(exp(coef(summary(model))['lr_change_modified_newchange:ldct_index3',1] + 
               qnorm(c(0.025, 0.975)) * coef(summary(model))['lr_change_modified_newchange:ldct_index3',4]))
-  print('Baseline LR 3-4, LR downgraded from T0 to T1, T0 vs T1')
-  print(exp(coef(summary(model))['baseline_lr_category3-4:lr_change_modified_newchange:ldct_index2',1] + 
-              qnorm(c(0.025, 0.975)) * coef(summary(model))['baseline_lr_category3-4:lr_change_modified_newchange:ldct_index2',4]))
-  print('Baseline LR 3-4, LR downgraded from T0 to T2, T0 vs T2')
-  print(exp(coef(summary(model))['baseline_lr_category3-4:lr_change_modified_newchange:ldct_index3',1] + 
-              qnorm(c(0.025, 0.975)) * coef(summary(model))['baseline_lr_category3-4:lr_change_modified_newchange:ldct_index3',4]))
+  print('Baseline LR 3to4, LR downgraded from T0 to T1, T0 vs T1')
+  print(exp(coef(summary(model))['baseline_lr_category3to4:lr_change_modified_newchange:ldct_index2',1] + 
+              qnorm(c(0.025, 0.975)) * coef(summary(model))['baseline_lr_category3to4:lr_change_modified_newchange:ldct_index2',4]))
+  print('Baseline LR 3to4, LR downgraded from T0 to T2, T0 vs T2')
+  print(exp(coef(summary(model))['baseline_lr_category3to4:lr_change_modified_newchange:ldct_index3',1] + 
+              qnorm(c(0.025, 0.975)) * coef(summary(model))['baseline_lr_category3to4:lr_change_modified_newchange:ldct_index3',4]))
 }
 
 # extract odds ratios for variables of interest only
 get_OR <- function(model){
-  print('Baseline LR 1-2, LR unchanged from T0 to T1, T0 vs T1')
+  print('Baseline LR 1to2, LR unchanged from T0 to T1, T0 vs T1')
   print(exp(coef(summary(model))['ldct_index2',1]))
-  print('Baseline LR 1-2, LR unchanged from T0 to T2, T0 vs T2')   
+  print('Baseline LR 1to2, LR unchanged from T0 to T2, T0 vs T2')   
   print(exp(coef(summary(model))['ldct_index3',1]))
-  print('Baseline LR 3-4, LR unchanged from T0 to T1, T0 vs T1')
-  print(exp(coef(summary(model))['baseline_lr_category3-4:ldct_index2',1]))
-  print('Baseline LR 3-4, LR unchanged from T0 to T2, T0 vs T2')
-  print(exp(coef(summary(model))['baseline_lr_category3-4:ldct_index3',1]))
-  print('Baseline LR 1-2, LR upgraded  from T0 to T1, T0 vs T1')
+  print('Baseline LR 3to4, LR unchanged from T0 to T1, T0 vs T1')
+  print(exp(coef(summary(model))['baseline_lr_category3to4:ldct_index2',1]))
+  print('Baseline LR 3to4, LR unchanged from T0 to T2, T0 vs T2')
+  print(exp(coef(summary(model))['baseline_lr_category3to4:ldct_index3',1]))
+  print('Baseline LR 1to2, LR upgraded  from T0 to T1, T0 vs T1')
   print(exp(coef(summary(model))['lr_change_modified_newchange:ldct_index2',1]))
-  print('Baseline LR 1-2, LR upgraded  from T0 to T2, T0 vs T2')
+  print('Baseline LR 1to2, LR upgraded  from T0 to T2, T0 vs T2')
   print(exp(coef(summary(model))['lr_change_modified_newchange:ldct_index3',1]))
-  print('Baseline LR 3-4, LR downgraded from T0 to T1, T0 vs T1')
-  print(exp(coef(summary(model))['baseline_lr_category3-4:lr_change_modified_newchange:ldct_index2',1]))
-  print('Baseline LR 3-4, LR downgraded from T0 to T2, T0 vs T2')
-  print(exp(coef(summary(model))['baseline_lr_category3-4:lr_change_modified_newchange:ldct_index3',1]))
+  print('Baseline LR 3to4, LR downgraded from T0 to T1, T0 vs T1')
+  print(exp(coef(summary(model))['baseline_lr_category3to4:lr_change_modified_newchange:ldct_index2',1]))
+  print('Baseline LR 3to4, LR downgraded from T0 to T2, T0 vs T2')
+  print(exp(coef(summary(model))['baseline_lr_category3to4:lr_change_modified_newchange:ldct_index3',1]))
 }
 
 # build gee model
 gee_analysis <- function(data){
+  # set ldct index as factor
+  data$ldct_index <- as.factor(data$ldct_index)
+  # reorder levels
+  data <- within(data, lr_change_modified_new <- relevel(lr_change_modified_new, ref = 2))
+  levels(data$lr_change_modified_new)
+  print(data$lr_change_modified_new)
+  
   # adjusted model
   model_complete_adjusted <- gee(adherence_altered ~
                                    baseline_lr_category*lr_change_modified_new*ldct_index +
@@ -85,22 +93,23 @@ gee_analysis <- function(data){
                                    baseline_adi_category+baseline_median_income_category+
                                    covid_expected_fu_date_lungrads_interval_new,
                                  data = data,
-                                 id = mrn,
+                                 id = research_id,
                                  family = binomial,
                                  maxiter = 100,
                                  corstr = "unstructured")
   print(2 * pnorm(abs(coef(summary(model_complete_adjusted))[,5]), lower.tail = FALSE))
   # get OR and 95% CI
+  print('OR')
   get_OR(model_complete_adjusted)
+  print('95% CI')
   get_CI(model_complete_adjusted)
 }
 
 # primary analysis - without imputation
-no_imputation_data = read.csv("path_to_file")
+no_imputation_data = read.csv("~/Dummy_Data_Experiment_3.csv")
 gee_analysis(no_imputation_data)
 
 # sensitivity analysis - with imputation
 imputed_data = read.csv("path_to_file")
 gee_analysis(imputed_data)
-
 
