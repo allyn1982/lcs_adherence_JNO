d056f4de9469827a48b40b68acf86ef75a028ff0
Yannan Lin <37195634+allyn1982@users.noreply.github.com>
2022-05-31 23:45:35 -0700

    Update model_test.py
    add comments

--- Experiment_2/model_test.py
+++ Experiment_2/model_test.py
@@ -7,12 +7,33 @@ class Model_test:
 
     """
     def build_final_model(self, classifier=None, model_type=None, file_path=None, file_type=None):
+        """Function to build final models.
+        Input:
+        classifier: specify the classifier
+        model_type: 'full' or 'simple'
+        file_path: path to training data
+        file_type: 'full_imputed' or 'simple_complete'
+        
+        Output:
+        A final full or simple model.
+        """
         final_model = classifier
         final_train_X, final_train_y = DataLoader(file_path, file_type=file_type).get_data(model_type=model_type)
         final_model.fit(final_train_X, final_train_y)
         return final_model
 
     def test_results(self, file_path=None,  file_type=None, model_type=None, classifier=None, test_path=None):
+        """Function to get results on test data.
+        Input:
+        file_path: path to training data
+        file_type: 'full_imputed' or 'simple_complete'
+        model_type: 'full' or 'simple'
+        classifier: specify the classifier
+        test_path: path to hold out test data
+        
+        Output:
+        Testing results for each metric.
+        """
         final_test_X, final_test_y = DataLoader(file_path=file_path, file_type=file_type).get_test_data(model_type=model_type, test_path=test_path)
         model = self.build_final_model(classifier=classifier, model_type=model_type, file_path=file_path, file_type=file_type)
         final_pred = model.predict(final_test_X)
