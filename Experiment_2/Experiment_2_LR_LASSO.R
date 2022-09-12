b6b71048f70b689de86387400a3f9fee076df9dc
Yannan Lin <37195634+allyn1982@users.noreply.github.com>
2022-09-12 14:02:17 -0700

    Update Experiment_2_LR_LASSO.R

--- Experiment_2/Experiment_2_LR_LASSO.R
+++ Experiment_2/Experiment_2_LR_LASSO.R
@@ -27,21 +27,14 @@ if(!require(glmnet)) install.packages('glmnet', repos = "http://cran.us.r-projec
 # read data
 my_data = read.csv('~/Dummy_Data_Experiment_2_LASSO.csv')
 # create a data matrix for predictors
-x <- model.matrix(my_data$adherence_altered ~
+x_exp_1_significant <- model.matrix(my_data$adherence_altered ~
                     my_data$lungrads_12_3_4+
-                    my_data$age_new+
-                    my_data$sex_new+
-                    my_data$race_ethnicity_new+
                     my_data$education_new+
-                    my_data$smoking_status_new+
                     my_data$fam_hx_lc_new+
                     my_data$comorbid_category_new+
                     my_data$department_new +
-                    my_data$distance_to_center_category_new+
                     my_data$adi_category_new+
-                    my_data$median_income_category_new+
-                    my_data$insurance_new+
-                    my_data$covid_expected_fu_date_lungrads_interval_new)[, -1]
+                    my_data$median_income_category_new)[, -1]
 
 # # alpha=1 is only lasso, alpha=0 is only ridge, otherwise both l1 and l2 norm
 # glmmod <- glmnet(x, y=as.factor(my_data$adherence_altered), alpha = 1, family = 'binomial')
@@ -51,7 +44,7 @@ x <- model.matrix(my_data$adherence_altered ~
 
 # use cv to select best lambda
 set.seed(5)
-cv.glmmod <- cv.glmnet(x, y=my_data$adherence_altered, alpha=1, nfolds = 10)
+cv.glmmod <- cv.glmnet(x_exp_1_significant, y=my_data$adherence_altered, alpha=1, nfolds = 10)
 plot(cv.glmmod)
 (best.lambda <- cv.glmmod$lambda.1se)
 # interpret results with cv - 1se
