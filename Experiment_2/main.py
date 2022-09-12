6b2a3770f3703aec77fa2301a7276ef2c277c158
Yannan Lin <37195634+allyn1982@users.noreply.github.com>
2022-09-12 15:02:12 -0700

    Update main.py

--- Experiment_2/main.py
+++ Experiment_2/main.py
@@ -11,17 +11,15 @@ def main(args=None):
     parser.add_argument('--fold', help='Specify k for cross validation. Use 3 if using dummy data.', type=int)
     parser.add_argument('--cv_or_test', help='Must be "cross_validation" or "test".')
     parser.add_argument('--train_data_type',
-                        help='Must be "full_complete", "full_imputed", "full_missing", "simple_complete", "simple_imputed", or "simple_missing". If testing final full model, use "full_complete". If testing final simple model, use "simple_imputed"')
+                        help='Must be "full_complete", "full_missing", "simple_complete", or "simple_missing". If testing final full model, use "full_complete". If testing final simple model, use "simple_imputed"')
 
     parser = parser.parse_args(args)
 
     # data paths
     full_complete_path = '~/lcs_adherence/Experiment_2/Dummy_Data/Dummy_Data_Experiment_2_full_complete.csv'
     full_missing_path = '~/lcs_adherence/Experiment_2/Dummy_Data/Dummy_Data_Experiment_2_full_missing.csv'
-    full_imputed_path = '~/lcs_adherence/Experiment_2/Dummy_Data/Dummy_Data_Experiment_2_full_imputed.csv'
     simple_complete_path = '~/lcs_adherence/Experiment_2/Dummy_Data/Dummy_Data_Experiment_2_simple_complete.csv'
     simple_missing_path = '~/lcs_adherence/Experiment_2/Dummy_Data/Dummy_Data_Experiment_2_simple_missing.csv'
-    simple_imputed_path = '~/lcs_adherence/Experiment_2/Dummy_Data/Dummy_Data_Experiment_2_simple_imputed.csv'
     test_data_path = '~/lcs_adherence/Experiment_2/Dummy_Data/Dummy_Data_Experiment_2_test_data.csv'
 
     # complete and imputed data
@@ -36,18 +34,12 @@ def main(args=None):
     else:
         print('------', parser.train_data_type, '------')
 
-    # cross validation on complete and imputed data sets
+    # cross validation on complete data sets
     if parser.cv_or_test == 'cross_validation' and parser.train_data_type == 'full_complete':
         X, y = DataLoader(full_complete_path).get_data(model_type='full')
         train_nested(X, y, cv_inner, cv_outer)
     elif parser.cv_or_test == 'cross_validation' and parser.train_data_type == 'simple_complete':
         X, y = DataLoader(simple_complete_path).get_data(model_type='simple')
-        train_nested(X, y, cv_inner, cv_outer)
-    elif parser.cv_or_test == 'cross_validation' and parser.train_data_type == 'full_imputed':
-        X, y = DataLoader(full_imputed_path).get_data(model_type='full')
-        train_nested(X, y, cv_inner, cv_outer)
-    elif parser.cv_or_test == 'cross_validation' and parser.train_data_type == 'simple_imputed':
-        X, y = DataLoader(simple_imputed_path).get_data(model_type='simple')
         train_nested(X, y, cv_inner, cv_outer)
 
     # cross validation on data with missing values
@@ -62,10 +54,10 @@ def main(args=None):
     if parser.cv_or_test == 'test' and parser.train_data_type == 'full_complete':
         test_results(classifier=LogisticRegression(),
                      model_type='full',
-                     file_path=full_imputed_path,
-                     file_type='full_imputed',
+                     file_path=full_complete_path,
+                     file_type='full_complete',
                      test_path=test_data_path)
-    elif parser.cv_or_test == 'test' and parser.train_data_type == 'simple_imputed':
+    elif parser.cv_or_test == 'test' and parser.train_data_type == 'simple_complete':
         test_results(classifier=LogisticRegression(),
                      model_type='simple',
                      file_path=simple_complete_path,
